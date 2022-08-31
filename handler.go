61d75ae22f2888443eec807f59f112aa15d8a988
Darius Mejeras <darius.mejeras@gmail.com>
2022-08-31 08:43:48 +0300

    Fix leftover

--- handler.go
+++ handler.go
@@ -22,11 +22,11 @@ func (h *procedureHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) { h
 func (h *procedureHandler) build(m *mux)                                     { h.http = h.builder(m) }
 
 type streamHandler struct {
-	builder     func(m *mux) http.HandlerFunc
-	httpHandler func(http.ResponseWriter, *http.Request)
+	builder func(m *mux) http.HandlerFunc
+	http    func(http.ResponseWriter, *http.Request)
 
 	meta
 }
 
-func (h *streamHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) { h.httpHandler(w, r) }
-func (h *streamHandler) build(m *mux)                                     { h.httpHandler = h.builder(m) }
+func (h *streamHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) { h.http(w, r) }
+func (h *streamHandler) build(m *mux)                                     { h.http = h.builder(m) }
